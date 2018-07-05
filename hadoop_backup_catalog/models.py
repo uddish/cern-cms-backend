512d70463620dba70b68caa55075df1810467682
uddish <uddish@squadplatform.com>
2018-07-05 11:26:22 +0200

    added all the models

--- hadoop_backup_catalog/models.py
+++ hadoop_backup_catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from mysite.settings import DATE_INPUT_FORMATS
 
-
+# add meta class to change the table's name
 class applications(models.Model):
     appid = models.IntegerField()
     appname = models.CharField(max_length=50)
@@ -16,6 +17,8 @@ class applications(models.Model):
     def __str__(self):
         return self.appname
 
+
+# add meta class to change the table's name
 class metadata(models.Model):
     hdfs_cluster = models.CharField(max_length=50)
     backupqueue = models.CharField(max_length=50)
@@ -25,6 +28,7 @@ class metadata(models.Model):
     def __str__(self):
         return self.namenode
 
+
 class backupsets(models.Model):
     appid = models.IntegerField()
     boid = models.IntegerField()
@@ -32,6 +36,92 @@ class backupsets(models.Model):
     backupset_name = models.CharField(max_length=256)
     status = models.CharField(max_length=24)
     num_files = models.IntegerField()
+
+    class Meta:
+        db_table = 'backupsets'
+
+    def __str__(self):
+        return self.appid
+
+
+class backupoperations(models.Model):
+    appid = models.IntegerField()
+    boid = models.IntegerField()
+    backup_type = models.CharField(max_length=9)
+    last_backup_timestamp = models.DateField()
+    num_archives = models.IntegerField()
+    start_time = models.DateField()
+    completion_time = models.DateField()
+    elapsed_seconds = models.IntegerField()
+    status = models.CharField(max_length=24)
+
+    class Meta:
+        db_table = 'backupoperations'
+
+    def __str__(self):
+        return self.appid
+
+
+class backupfile_exceptions(models.Model):
+    appid = models.IntegerField()
+    boid = models.IntegerField()
+    bsid = models.IntegerField()
+    file_name = models.CharField(max_length=1000)
+    file_size = models.IntegerField()
+    resent = models.CharField(max_length=12)
+    r_boid = models.IntegerField()
+    r_bsid = models.IntegerField()
+    exception_reason = models.CharField(max_length=64)
+    resent_reason = models.CharField(max_length=64)
+
+    class Meta:
+        db_table = 'backupfile_exceptions'
+
+    def __str__(self):
+        return self.appid
+
+
+# class backuparchives_raw(models.Model):
+#     appid = models.IntegerField()
+#     boid = models.IntegerField()
+#     bsid = models.IntegerField()
+#     file_name = models.CharField(max_length=1000)
+#     file_size = models.IntegerField()
+#     status = models.CharField(max_length=24)
+#     handle_raw = models.CharField(max_length=200)
+#     last_seen = models.DateField()
+#
+#     class Meta:
+#         db_table = 'backuparchives_raw'
+#
+#     def __str__(self):
+#         return self.appid
+
+class backuparchives(models.Model):
+    appid = models.IntegerField()
+    boid = models.IntegerField()
+    bsid = models.IntegerField()
+    backup_type = models.CharField(max_length=9)
+    file_name = models.CharField(max_length=1000)
+    filename_offset = models.IntegerField()
+    length = models.IntegerField()
+    handle = models.CharField(max_length=256)
+    backupfile_offset = models.IntegerField()
+    status = models.CharField(max_length=24)
+
+    class Meta:
+        db_table = 'backuparchives'
+
+    def __str__(self):
+        return self.appid
+
+
+class exclusion_list(models.Model):
+    appid = models.IntegerField()
+    excl_list = models.CharField(max_length=512)
+
+    class Meta:
+        db_table = 'exclusion_list'
 
     def __str__(self):
         return self.appid
