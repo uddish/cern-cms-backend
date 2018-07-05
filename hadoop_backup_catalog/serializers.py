be0f19eac596e8b93182331751e3cd183d26dc23
uddish <uddish@squadplatform.com>
2018-07-05 12:05:23 +0200

    created serializers

--- hadoop_backup_catalog/serializers.py
+++ hadoop_backup_catalog/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import metadata, applications
+from .models import metadata, applications, backupsets, backupoperations, backupfile_exceptions,backuparchives_raw, backuparchives, exclusion_list
 
 class MetadataSerializer(serializers.ModelSerializer):
 
@@ -7,8 +7,51 @@ class MetadataSerializer(serializers.ModelSerializer):
         model = metadata
         fields = '__all__'
 
+
 class ApplicationsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = applications
         fields = '__all__'
+
+
+class BackupsetsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = backupsets
+        field = '__all__'
+
+
+class BackupoperationsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = backupoperations
+        field = '__all__'
+
+
+class BackupfileExceptionsSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = backupfile_exceptions
+        field = '__all__'
+
+
+class BackuparchivesRawSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = backuparchives_raw
+        field = '__all__'
+
+
+class BackuparchivesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = backuparchives
+        field = '__all__'
+
+
+class ExclusionListSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = exclusion_list
+        field = '__all__'
